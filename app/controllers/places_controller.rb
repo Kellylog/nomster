2f6c622becacb4e15e5694dc0297bcab677d7ad1
Kelly Kwak <kellylogblog@gmail.com>
2016-03-25 13:19:56 +0000

    pagination

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,6 +2,13 @@ class PlacesController < ApplicationController
 
   def index
     @places = Place.all
+    
   end
   
+  def index
+    @places = Place.order(:name).page(params[:page]).per(5)
+  end
+
+
+
 end
