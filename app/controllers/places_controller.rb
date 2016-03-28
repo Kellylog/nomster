f0bb31be94d9555b51adac19a851d0f072e67df3
vagrant <vagrant@precise32.(none)>
2016-03-28 08:29:57 +0000

    simple form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,15 +1,13 @@
 class PlacesController < ApplicationController
 
-  
-  
-
   def index
     @places = Place.order(:name).page(params[:page]).per(5)
   end
 
 
-def new
-end
+  def new
+    @place = Place.new
+  end
 
 
 end
