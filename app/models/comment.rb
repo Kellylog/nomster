110839facea04682da73c791f538a8b762c6a022
kellylog <kellylogblog@gmail.com>
2016-04-01 13:11:21 +0000

    rating

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,7 @@ class Comment < ActiveRecord::Base
 
   RATINGS = {
     'one star' => '1_star',
-    'two stars' => '2_stars',
+    '★★' => '2_stars',
     'three stars' => '3_stars',
     'four stars' => '4_stars',
     'five stars' => '5_stars',
@@ -13,5 +13,5 @@ class Comment < ActiveRecord::Base
   def humanized_rating
     RATINGS.invert[self.rating]
   end
-  
+
 end
